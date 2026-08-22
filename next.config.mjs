a540ce93231976e2b4b229f9243db7903f552f30
benjaminosw <benjaminongsw@gmail.com>
2026-08-22 21:35:49 +0800

    Added deploy workflow for GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+/** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  basePath: isGitHubPages ? '/AstroAlign' : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
